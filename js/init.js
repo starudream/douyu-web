e15d6d94be7aaacbd9d3c41e49f4095b0255a0c9
sheng.wang <justwangsheng@gmail.com>
2020-03-18 20:03:13 +0800

    feat: add tab

--- js/init.js
+++ js/init.js
@@ -11,6 +11,7 @@ var _gift = document.getElementById('gift');
 var _user = document.getElementById('user');
 var _start = document.getElementById('start');
 var _close = document.getElementById('close');
+var _msg = document.getElementById('msg');
 var _msg1 = document.getElementById('msg1');
 var _msg2 = document.getElementById('msg2');
 var _msg3 = document.getElementById('msg3');
