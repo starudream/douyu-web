77a66385b708ae1dad724ee08954755e7eddec11
StarUDream <justwangsheng@qq.com>
2020-03-23 10:19:44 +0800

    fix: fix version

--- js/version.js
+++ js/version.js
@@ -8,13 +8,16 @@ var version = '';
             success: function (resp) {
                 if (resp.tag_name !== version && resp.assets && resp.assets.length > 0) {
                     var body = resp.body.replace('\n', '<br>');
+                    var asset = resp.assets[0];
                     layer.confirm(
-                        '当前版本 <b>' + version + '</b>，最新版本 <b>' + resp.tag_name + '</b><hr>更新说明：<br>' + body,
+                        '当前版本 <b>' + version + '</b>，最新版本 <b>' + resp.tag_name + '</b><br>' +
+                        '更新大小 <b>' + (asset.size / 1024).toFixed(3) + '</b> KB' +
+                        '<hr>更新说明：<br>' + body,
                         {
                             btn: ['立即下载', '稍后提示'],
                             yes: function (i) {
                                 layer.close(i);
-                                window.open(resp.assets[0].browser_download_url);
+                                window.open(asset.browser_download_url);
                             },
                             btn2: function (i) {
                                 layer.close(i);
