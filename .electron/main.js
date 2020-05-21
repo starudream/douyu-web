91806a8a624940297e3a70605741831adbcbee77
starudream <justwangsheng@qq.com>
2020-05-21 14:01:35 +0800

    fix: fix

--- .electron/main.js
+++ .electron/main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
-        height: 800,
+        height: 600,
         autoHideMenuBar: true,
     });
 
